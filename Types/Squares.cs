f12ecfa017738166b3aceb7ca032617852715980
Sabit Igde <sabitertan@gmail.com>
2019-11-17 21:02:23 -0500

    Initial commit

---
+++ Types/Squares.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTicTacToe.Types
+{
+    public class Squares
+    {
+        public string[] Items { get; set; }
+    }
+}
